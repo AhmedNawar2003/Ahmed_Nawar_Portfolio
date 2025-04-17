1b01603965090396c486077bd5af56730eab4a8e
Ahmed Nawar <153510555+AhmedNawar2003@users.noreply.github.com>
2025-04-17 12:59:16 +0200

    changes

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -2,13 +2,12 @@ import { FaLaptopCode } from "react-icons/fa";
 
 export default function Courses() {
   return (
-    <section className="py-12  text-white">
+    <section className="py-12 text-white">
       <div className="container mx-auto px-4">
         <h2 className="md:text-3xl font-bold flex items-center gap-2 mb-8">
           <FaLaptopCode className="text-green-400" />
           Courses
         </h2>
-
         <div className="relative border-l border-slate-700 pl-6">
           <div className="mb-10 relative">
             <div className="absolute left-[-18px] top-2 w-3 h-3 bg-green-500 rounded-full border-4 border-gray-950"></div>
