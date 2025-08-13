1d87de0926b2cc7313c4db206408209534ceec95
Ahmed Nawar <153510555+AhmedNawar2003@users.noreply.github.com>
2025-08-13 11:47:51 +0300

    update

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -13,13 +13,13 @@ export default function Education() {
             <div className="mb-10 relative">
               <div className="absolute left-[-18px] top-2 w-3 h-3 bg-blue-500 rounded-full border-4 border-gray-950"></div>
               <h3 className="md:text-xl font-semibold">
-                Faculty of Computers and Information
+                Bachelor of Computers and Information
               </h3>
               <p className="text-slate-400 mt-1">
                 Monufia University • 2021 – 2025
               </p>
               <p className="text-slate-400 mt-1">
-                GPA: <span className="font-medium text-white">3.45</span>
+                Grade: <span className="font-medium text-white me-2">Very Good (B+)</span> GPA: <span className="font-medium text-white">3.45</span>
               </p>
             </div>
           </div>
